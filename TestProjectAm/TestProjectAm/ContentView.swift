cca2ae1c169b6ee900e646ca6b2f9e6885ac39b8
a.m.student <asumehrocks@gmail.com>
2021-01-20 10:09:09 -0600

    Added button

---
+++ TestProjectAm/TestProjectAm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestProjectAm
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Greetings, planetary lifeland.")
+            .padding()
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProjectAm/TestProjectAm/TestProjectAmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectAmApp.swift
+//  TestProjectAm
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectAmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
